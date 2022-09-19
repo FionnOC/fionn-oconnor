0860b15778dc0b7717a35eb5489e8bdd2c61727a
Fionn <fionn010@gmail.com>
2022-09-19 22:37:30 +0100

    fix: changed description on home page and started to add work experiences on new page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import Head from 'next/head';
-import Link from 'next/link';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
+import Head from "next/head";
+import Link from "next/link";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
 
 export default function Home() {
   return (
@@ -10,12 +10,25 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Fourth Year Computer Engineering student in TCD, who loves climbing, photography, and design</p>
+        <h3>Hi there! Welcome to my website :) </h3>
+        <p>
+          I am a fourth-year student at Trinity College Dublin studying computer
+          engineering who is a well-rounded, positive, and ambitious person.
+        </p>
+        <p>
+          Over the past few years, I have worked in many different roles, from
+          trainee to supervisor, and due to my previous experiences and my
+          computer engineering studies, I have great problem-solving skills.{" "}
+        </p>
+        <p>
+          I am seeking an internship that would allow me to apply my skills and
+          experiences, while also fostering my love for technology.
+        </p>
         {/* <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p> */}
-        <Link href="/posts/first-post">Work Experience</Link>
+        <Link href="/posts/work-experience">Work Experience</Link>
       </section>
     </Layout>
   );
